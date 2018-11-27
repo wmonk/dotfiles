406b938b8c7ebd0e4050346b3d73885fbd037515
William Monk <will@passfort.com>
2018-11-27 19:19:57 +0000

    Done

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,10 +1,12 @@
-. ~/.config/z-fish/z.fish
-source ~/.config/git-aliases.fish
-source ~/.config/fish/df.fish
+if not status --is-interactive
+  exit
+end
 
+set -U TERM xterm-256coor
 export EDITOR=nvim
-export PYTHONPATH=$HOME/Code/PassFort
-export PASSFORT_ROOT=$HOME/Code/PassFort
+export PYTHONPATH=$HOME/Code/minifort/services/passfort
+export PASSFORT_ROOT=$HOME/Code/minifort/services/passfort
+export RUST_SRC_PATH=/Users/will/.rustup/toolchains/nightly-x86_64-apple-darwin/lib/rustlib/src/rust/src
 
 function tree
     find $argv[1] -print | sed -e 's;[^/]*/;|____;g;s;____|; |;g'
@@ -12,50 +14,9 @@ end
 alias fishconfig="vim ~/.config/fish/config.fish"
 alias vim="nvim"
 
-function hot-reload
-    echo "Applying hot-reloading commit, fingers crossed 🔥"
-    git cherry-pick cf0c890a2cac670b50547eed04edfa2c965fa9f9
-end
-
-function aroundline
-    set -l file (string split ':' $argv[1])
-    set -l args (count $argv)
-    if test $args -eq 2
-        set around $argv[2]
-    else
-        set around 10
-    end
-    showlines (math $file[2] - $around) (math $file[2] + $around) $file[1]
-end
-
-function showlines
-    cat $argv[3] | awk "NR >= $argv[1] && NR <= $argv[2]"
-end
-
-function git-freeze
-    git commit -am 'FROZEN'
-end
-
-function git-unfreeze
-    set -l matched (string match -r "FROZEN" (git log --oneline -n 1))
-    if test -n "$matched"
-        git reset --soft HEAD^
-    else
-        echo "No frozen commit found"
-    end
-end
-
-function gcb
-    git commit -m (string join "" "[" (git branch ^/dev/null | sed -n '/\* /s///p') "]" " " $argv[1])
-end
-
-# eval sh ~/.config/base16-shell/base16-solarized.dark.sh
-# eval sh ~/.config/base16-shell/base16-ocean.light.sh
-
 # nodejs
 set -g NODE_PATH "$HOME/.npm-packages/lib/node_modules" $NODE_PATH
-set -g PATH "./node_modules/.bin" $PATH
-set -g PATH "/usr/local/sbin" $PATH
+set -g PATH "/usr/local/sbin" "./node_modules/.bin" $PATH
 
 set __fish_git_prompt_showdirtystate 'yes'
 set __fish_git_prompt_describe_style 'branch'
@@ -77,124 +38,9 @@ function rm
     command rm -i $argv
 end
 
-alias vpn='sudo -p "sudo password: " openconnect -g SKY-MINI-TOKEN -u $LOGNAME -m 1000 https://skyremoteaccess.bskyb.com'
 alias lll='ls -1 | less'
 
-#function fish_mode_prompt
-#end
-
-function fishy_vi_prompt
-if set -q __fish_vi_mode
-    switch $fish_bind_mode
-      case default
-        set_color red
-        echo '◉'
-      case insert
-        set_color green
-        echo '◉'
-      case replace-one
-        set_color green
-        echo '◉'
-      case visual
-        set_color magenta
-        echo '◉'
-    end
-    set_color normal
-    echo -n ' '
-  end
-end
-
-function fish_right_prompt
-    __fish_git_prompt
-end
-
-function gh --description 'Open the webpage for the current github repo/branch'
-    set -l fetch_url (command git remote --verbose show -n origin ^/dev/null | command grep Fetch | cut -c 18- | string replace : / | rev | cut -c 5- | rev)
-
-    #did we get an exit status?
-    if [ $status -gt 0 ]
-        echo 'Not a git repo.'
-        return 1
-    end
-
-    if [ -z $fetch_url ]
-        echo 'Not a git repo.'
-        return 1
-    end
-
-    if [ -z (echo $fetch_url | grep github ) ]
-        echo 'Not a github repo.'
-        return 3
-    end
-
-    if [ (string match (git remote -v | awk '{ print $2 }') 'https' = 'https') ]
-        open (git remote -v | awk '{ print $2 }')
-    end
-
-    set -l branch (command git rev-parse --abbrev-ref HEAD)
-
-    if [ $branch = 'HEAD' ]
-        # we couldn't find a branch or tag, so lets get a sha
-        set branch (command git rev-parse HEAD)
-    end
-
-    set url (echo "$fetch_url/tree/$branch" | sed 's|git@github.com:\(.*\)\.git|https://github.com/\1|')
-
-    open "http://$url/$argv"
-end
-
-alias vimconfig="nvim ~/.config/nvim/ts.vim"
-
-test -e {$HOME}/.iterm2_shell_integration.fish ; and source {$HOME}/.iterm2_shell_integration.fish
-
-function vf
-  set files (locate -Ai -0 $argv | grep -z -vE '~$' | fzf --read0 -0 -1 -m)
-
-  if [[ -n $files ]]
-     vim -- $files
-     print -l $files[1]
-  end
-end
-
-set new_shell 1
-
-function __check_pipenv --description 'Do pipenv stuff' --on-variable PWD
-    status --is-command-substitution
-    and return
-
-    if test $new_shell -eq 1
-        set new_shell 0
-        and return
-    end
-
-    set -l GIT_DIR (git rev-parse --git-dir 2> /dev/null)
-
-    if test -e "$GIT_DIR"
-        if test ".git" = $GIT_DIR
-            set GIT_DIR (pwd)/.git
-        end
-        set -l PROJECT_ROOT (dirname $GIT_DIR)
-        set -l VENV_FILE $PROJECT_ROOT/.venv
-        set -gx PYTHONPATH (pwd)
-
-        if [ -f $VENV_FILE ]
-            and test "$VIRTUAL_ENV" = ""
-            echo "Found .venv file "
-            # pipenv --venv
-            source (pipenv --venv)/bin/activate.fish
-        end
-    else
-        if test "$VIRTUAL_ENV" != ""
-            echo "Left venv, deactivating"
-            deactivate
-        end
-    end
-end
-
 abbr br="git branch ^/dev/null | sed -n '/\* /s///p'"
-
-# The next line updates PATH for the Google Cloud SDK.
-if [ -f '/Users/will/Downloads/google-cloud-sdk/path.fish.inc' ]; if type source > /dev/null; source '/Users/will/Downloads/google-cloud-sdk/path.fish.inc'; else; . '/Users/will/Downloads/google-cloud-sdk/path.fish.inc'; end; end
 
 set -x PATH ~/.cargo/bin $PATH
 set -x PATH ~/.yarn/bin $PATH
@@ -202,9 +48,58 @@ set -x PATH ~/.yarn/bin $PATH
 alias fvim="vim (fzf)"
 alias recent="git checkout (git reflog | egrep -io 'moving from ([^[:space:]]+)' | awk '{ print \$3 }' | awk ' !x[\$0]++' | fzf)"
 
-set -gx PATH "/Users/will/.opam/system/bin" $PATH;
-set -gx OCAML_TOPLEVEL_PATH "/Users/will/.opam/system/lib/toplevel";
-set -gx PERL5LIB "/Users/will/.opam/system/lib/perl5:$PERL5LIB";
-set -gx MANPATH "$MANPATH" "/Users/will/.opam/system/man";
-set -gx OPAMUTF8MSGS "1";
-set -gx CAML_LD_LIBRARY_PATH "/Users/will/.opam/system/lib/stublibs:/usr/local/lib/ocaml/stublibs";
+alias c="fzf-cd-widget"
+alias git=hub
+alias json="pbpaste | jq . -C | less -r"
+
+abbr gcl='git clone'
+abbr ga='git add'
+abbr gap='git add -p'
+abbr gf='git fetch --all --prune'
+abbr gm="git merge"
+abbr g='git'
+abbr gs='git status'
+abbr gl='git pull'
+abbr gr='git rebase'
+abbr grc='git rebase --continue'
+abbr grom='git rebase origin/master'
+abbr gros='git rebase origin/staging'
+abbr gp='git push'
+abbr gpu='git push --set-upstream'
+abbr gc='git commit -v'
+abbr gcm='git commit -v -m'
+abbr gb='git branch'
+abbr gba='git branch -a'
+abbr gcp='git cherry-pick'
+abbr gco='git checkout'
+abbr gcb='git checkout -b'
+abbr gdel='git branch -D'
+abbr glo="git log --oneline"
+abbr gnew="git log HEAD@{1}..HEAD@{0}"
+function gd
+    git diff --color $argv | diff-so-fancy  | less --tabs=4 -RFX
+end
+abbr gds="git diff --staged"
+abbr gsp="git stash pop"
+abbr gst="git stash"
+abbr gca="git commit --amend"
+abbr gcane="git commit --amend --no-edit"
+
+function fish_right_prompt
+    __fish_git_prompt
+end
+
+# Defined in /Users/will/.config/fish/functions/fish_prompt.fish @ line 1
+function fish_prompt
+  test $SSH_TTY
+  and printf (set_color red)$USER(set_color brwhite)'@'(set_color yellow)(prompt_hostname)' '
+  test $USER = 'root'
+  and echo (set_color red)"#"
+
+  # Main
+  echo -n (set_color brblack)(date -j "+%H:%M:%S") (set_color cyan)(prompt_pwd) (set_color red)'❯'(set_color yellow)'❯'(set_color green)'❯ '
+end
+
+# if type -q fizzygit
+#     fizzygit
+# end
--- .config/nvim/init.vim
+++ .config/nvim/init.vim
@@ -19,9 +19,24 @@ set autoindent
 set smartindent
 set incsearch
 set inccommand=split
+set foldmethod=manual
+
+let g:loaded_python_provider = 1
+let g:python_host_prog='/usr/local/bin/python'
+let g:python3_host_prog='/usr/local/bin/python3'
+
+let &t_Cs = "\e[4:3m"
+let &t_Ce = "\e[4:3m"
+hi SpellBad gui=undercurl guisp=red term=undercurl cterm=undercurl
 
 call plug#begin('~/.vim/plugged')
 
+Plug 'nightsense/stellarized'
+Plug 'iloginow/vim-stylus'
+Plug 'cormacrelf/vim-colors-github'
+Plug 'AndrewRadev/switch.vim'
+Plug 'airblade/vim-gitgutter'
+Plug 'FooSoft/vim-argwrap'
 Plug 'zooxyt/Ultisnips-rust'
 Plug 'tomtom/tcomment_vim'
 Plug 'rust-lang/rust.vim'
@@ -43,12 +58,18 @@ Plug 'autozimu/LanguageClient-neovim', {
     \ 'branch': 'next',
     \ 'do': 'bash install.sh',
     \ }
-Plug 'roxma/nvim-completion-manager'
+Plug 'ncm2/ncm2'
+Plug 'ncm2/ncm2-bufword'
+Plug 'ncm2/ncm2-tmux'
+Plug 'ncm2/ncm2-path'
+Plug 'ncm2/ncm2-ultisnips'
+Plug 'roxma/nvim-yarp'
 Plug 'SirVer/ultisnips'
 Plug 'alexbyk/vim-ultisnips-js-testing'
 Plug 'wmonk/vim-react-snippets'
 Plug 'SirVer/ultisnips'
 Plug 'epilande/vim-es2015-snippets'
+Plug 'bogado/file-line'
 
 call plug#end()
 
@@ -84,20 +105,25 @@ function! Agerium()
   execute 'Ag ' . params
 endfunction
 map <Leader>i :call Agerium()<CR>
+vmap <Leader>i y:Ag <C-R>"<CR>
 map <Leader>g :GFiles?<CR>
 map <Leader>b :Buffers<CR>
 map <Leader>l :Lines<CR>
 
+    " \ 'javascript': ['javascript-typescript-stdio', '-l', '~/Code/tmp/ll'],
+    " \ 'javascript.jsx': ['javascript-typescript-stdio', '-l', '~/Code/tmp/ll'],
 let g:LanguageClient_serverCommands = {
     \ 'rust': ['~/.cargo/bin/rustup', 'run', 'stable', 'rls'],
-    \ 'javascript': ['flow-language-server', '--stdio'],
     \ 'python': ['pyls', '-vv'],
+    \ 'javascript': ['flow', 'lsp', '--from', './node_modules/.bin'],
+    \ 'javascript.jsx': ['flow', 'lsp', '--from', './node_modules/.bin'],
     \ 'typescript': ['typescript-language-server', '--stdio'],
     \ 'reason': ['/Users/will/Downloads/reason-language-server/reason-language-server.exe']
     \ }
 let g:LanguageClient_autoStart = 1
 
 nnoremap <silent> <Leader>T :call LanguageClient_textDocument_hover()<CR>
+nnoremap <silent> <Leader>D :vsplit<CR><C-W>l:call LanguageClient_textDocument_definition()<CR>
 nnoremap <silent> <Leader>d :call LanguageClient_textDocument_definition()<CR>
 nnoremap <silent> <Leader>r :call LanguageClient_textDocument_rename()<CR>
 
@@ -162,3 +188,36 @@ augroup END
 
 let g:LanguageClient_loggingFile = '/tmp/LanguageClient.log'
 let g:LanguageClient_serverStderr = '/tmp/LanguageServer.log'
+
+nnoremap <silent> <leader>a :ArgWrap<CR>
+
+autocmd FileType rust let b:switch_custom_definitions =
+      \ [
+      \   {
+      \     '&\(.\+\)':        '\1'
+      \   }
+      \ ]
+
+
+highlight ExtraWhitespace guisp=red
+match ExtraWhitespace /\s\+$/
+autocmd BufWinEnter * match ExtraWhitespace /\s\+$/
+autocmd InsertEnter * match ExtraWhitespace /\s\+\%#\@<!$/
+autocmd InsertLeave * match ExtraWhitespace /\s\+$/
+autocmd BufWinLeave * call clearmatches()
+
+" Disable tmux navigator when zooming the Vim pane
+let g:tmux_navigator_disable_when_zoomed = 1
+
+
+" ncm2
+autocmd BufEnter  *  call ncm2#enable_for_buffer()
+set completeopt=noinsert,menuone,noselect
+inoremap <expr> <CR> (pumvisible() ? "\<c-y>\<cr>" : "\<CR>")
+
+" Use <TAB> to select the popup menu:
+inoremap <expr> <Tab> pumvisible() ? "\<C-n>" : "\<Tab>"
+inoremap <expr> <S-Tab> pumvisible() ? "\<C-p>" : "\<S-Tab>"
+
+au BufNewFile,BufRead,BufReadPost Pipfile set ft=cfg
+
